8f7ec24d3c108e01ad959f60cdec918ca9aea1c1
Alexis Gilard <alexis.gilard@gmail.com>
2018-02-08 12:09:28 +0100

    Fix tinymce config

--- js/tinymce_config.js
+++ js/tinymce_config.js
@@ -1,10 +1,11 @@
-function (ed) {
+function(ed){
+
 	ed.on('init', function(e){
 		toot_editor.init(e);
-	});
+	})
 
 	ed.on('keyup', function(e){
+		console.log('keyup');
 		toot_editor.generate_toot();
-	});
-
+	})	
 }
